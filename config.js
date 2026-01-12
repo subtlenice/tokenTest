75799ed1f8d38563b44aea80631488fe3bc8ca00
subtlenice <32807406+subtlenice@users.noreply.github.com>
2026-01-12 17:32:10 +0400

    Create config.js
    Config file of Style Dictionary

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  source: ["*.json"],
+  platforms: {
+    web: {
+      transformGroup: "css",
+      buildPath: "build/web/",
+      files: [{ destination: "variables.css", format: "css/variables" }]
+    },
+    ios: {
+      transformGroup: "ios-swift",
+      buildPath: "build/ios/",
+      files: [{ destination: "StyleTokens.swift", format: "ios-swift/class.swift" }]
+    },
+    android: {
+      transformGroup: "android",
+      buildPath: "build/android/",
+      files: [{ destination: "colors.xml", format: "android/resources" }]
+    }
+  }
+};
